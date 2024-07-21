a6aa3afe83d4f08cf8d6f2e6c8c7ca4f0d89bf41
yahyazx4 <elbouatmani123@gmail.com>
2024-07-21 09:55:16 +0100

    Checkpoints

---
+++ test.py
@@ -0,0 +1,19 @@
+#Age = 21 
+#print(f"hello my name is Yahya and i am" ,Age, "years old")
+
+
+#integerage 
+#age = 20 
+#family = 5 
+#money = 12000
+
+#print("i am" , age , "years old")
+#print("in family we are" ,family,)
+#print("in my bank account i have" ,money, "DH")
+
+#Float
+
+distance = 2.5 
+vacation = 3.5 
+
+'true' and 'True'
